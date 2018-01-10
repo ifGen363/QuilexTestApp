debfb38be40a91e493aaff6da858df2a3138c969
QULIX\martulei <ifgen363@gmail.com>
2018-01-10 11:46:57 +0300

    Remove gif loading from adapters

--- app/src/main/java/org/qtestapp/adapters/CachedGifsRecyclerViewAdapter.kt
+++ app/src/main/java/org/qtestapp/adapters/CachedGifsRecyclerViewAdapter.kt
@@ -2,28 +2,22 @@ package org.qtestapp.adapters
 
 import android.content.Context
 import android.support.annotation.LayoutRes
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import com.bumptech.glide.Glide
 import org.qtestapp.R
+import org.qtestapp.adapters.holders.CachedGifItemViewHolder
 import org.qtestapp.cache.GifCache
-import java.io.File
-import java.net.URI
 
-/**
- * Created by MartulEI on 08.01.2018.
- */
+
 class CachedGifsRecyclerViewAdapter(
         val context: Context,
         @LayoutRes private val layoutRes: Int,
-        private val gifCache: GifCache) : BaseRecyclerViewAdapter<CachedGifsRecyclerViewAdapter.CachedGifItemViewHolder, String>() {
+        private val gifCache: GifCache) : BaseRecyclerViewAdapter<CachedGifItemViewHolder, String>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): CachedGifItemViewHolder =
-            CachedGifItemViewHolder(LayoutInflater.from(parent?.context).inflate(layoutRes, parent, false))
+            CachedGifItemViewHolder(LayoutInflater.from(parent?.context).inflate(layoutRes, parent, false),
+                                    R.id.gifImageView)
 
 
     override fun onBindViewHolder(holder: CachedGifItemViewHolder?, position: Int) {
@@ -38,20 +32,6 @@ class CachedGifsRecyclerViewAdapter(
 
                 gifCache.get(data[position])?.file?.toURI()?.let { uri -> loadGifFromCache(context, uri) }
             }
-        }
-    }
-
-
-    class CachedGifItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        private val gifImageView: ImageView = itemView.findViewById(R.id.gifImageView)
-        val deleteImageView: ImageView = itemView.findViewById(R.id.deleteImageView)
-
-        fun loadGifFromCache(context: Context, uri: URI) {
-            Glide
-                    .with(context)
-                    .load(File(uri))
-                    .into(gifImageView)
         }
     }
 }
--- app/src/main/java/org/qtestapp/adapters/GifsRecyclerViewAdapter.kt
+++ app/src/main/java/org/qtestapp/adapters/GifsRecyclerViewAdapter.kt
@@ -2,25 +2,19 @@ package org.qtestapp.adapters
 
 import android.content.Context
 import android.support.annotation.LayoutRes
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.CheckBox
-import android.widget.ImageView
-import com.bumptech.glide.Glide
 import org.qtestapp.R
+import org.qtestapp.adapters.holders.GifItemViewHolder
 import org.qtestapp.cache.GifCache
 import org.qtestapp.rest.model.response.GifData
-import java.io.File
-import java.net.URI
 
 
 class GifsRecyclerViewAdapter(private val context: Context,
                               @LayoutRes private val layoutRes: Int,
                               private val gifCache: GifCache,
                               private var cacheAction: (id: String, url: String) -> Unit) :
-        BaseRecyclerViewAdapter<GifsRecyclerViewAdapter.GifItemViewHolder, GifData>() {
+        BaseRecyclerViewAdapter<GifItemViewHolder, GifData>() {
 
 
     override fun onBindViewHolder(holder: GifItemViewHolder?, position: Int) {
@@ -50,26 +44,7 @@ class GifsRecyclerViewAdapter(private val context: Context,
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): GifItemViewHolder =
-            GifItemViewHolder(LayoutInflater.from(parent?.context).inflate(layoutRes, parent, false))
+            GifItemViewHolder(LayoutInflater.from(parent?.context).inflate(layoutRes, parent, false),
+                              R.id.gifImageView)
 
-
-    class GifItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        private val gifImageView: ImageView = itemView.findViewById(R.id.gifImageView)
-        val isCachedCheckBox: CheckBox = itemView.findViewById(R.id.isCachedCheckBox)
-
-        fun loadGifFromUrl(context: Context, url: String) {
-            Glide
-                    .with(context)
-                    .load(url)
-                    .into(gifImageView)
-        }
-
-        fun loadGifFromCache(context: Context, uri: URI) {
-            Glide
-                    .with(context)
-                    .load(File(uri))
-                    .into(gifImageView)
-        }
-    }
 }
